32d51f11a6706323ff49c77afb66df39271a47a2
gcorp-git <gcorp.gcorp@gmail.com>
2022-10-21 03:16:08 +0300

    configs

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,10 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.test.ts'],
-  experimentalDecorators: true,
   globals: {
     'ts-jest': {
-      tsConfig: 'test/tsconfig.json'
+      tsconfig: 'test/tsconfig.json'
     }
   }
 };
